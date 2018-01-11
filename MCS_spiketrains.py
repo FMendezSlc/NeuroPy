38cd4ca84cd3c3f76ffe3ff6b706ccb27dbffad6
FMendezSlc <fmendezslc@gmail.com>
2018-01-11 16:50:33 -0600

    improvements in block construction
    Better function for neo.Block construction
    Think about Units and ChannelIndex

--- MCS_spiketrains.py
+++ MCS_spiketrains.py
@@ -1,22 +1,13 @@
 import numpy as np
-import scipy as sp
 import neo
 import matplotlib.pyplot as plt
 import pandas as pd
 from quantities import Hz, s, ms
 from elephant.conversion import BinnedSpikeTrain
 from elephant.spike_train_correlation import corrcoef
-import seaborn as sb
+import seaborn as sns
 import elephant.statistics as est
 import elephant.spike_train_correlation as sp_cor
-import os as os
-
-
-cd '/Users/felipeantoniomendezsalcido/Desktop/MEAs/data_files'
-ls
-data_file = os.listdir()[2]
-raw_data = pd.read_csv(data_file, sep=',', header=0, usecols=[0, 1, 2])
-ord_times = raw_data.groupby(['Channel', 'Unit'])['Timestamp']
 
 
 def build_block(data_file):
@@ -28,39 +19,50 @@ def build_block(data_file):
     raw_data = pd.read_csv(data_file, sep=',', header=0, usecols=[0, 1, 2])
     ord_times = raw_data.groupby(['Channel', 'Unit'])['Timestamp']
     new_block = neo.Block()
+    # Next line will not work properly if last spike happens
+    # exactly at the end of the recording
     num_segments = range(int(raw_data['Timestamp'].max() // 600 + 1))
     for ind in num_segments:
         seg = neo.Segment(name='segment {}'.format(ind), index=ind)
         new_block.segments.append(seg)
 
     for name, group in ord_times:
-        st = ord_times.get_group(name).values
-        inter = 600
-        first_seg = neo.SpikeTrain(st[st < inter], units='sec', t_stop=inter)
+        time_stamps = ord_times.get_group(name).values
+        inter = 600  # Number of seconds in 10 minutes
+        first_seg = neo.SpikeTrain(time_stamps[time_stamps < inter], units='sec', t_stop=inter)
         new_block.segments[0].spiketrains.append(first_seg)
-        seg_count = 1
 
-        for seg in num_segments:
-            seg_count += 1
-            if seg_count < num_segments[-1]:
-                seg_train = neo.SpikeTrain(st[(st > (seg_count - 1 * inter)) & (st < (seg_count * inter))],
-                                           units='sec', t_start=(seg_count - 1 * inter), t_stop=(seg_count * inter))
-                new_block.segments[seg_count].spiketrains.append(seg_train)
+        for seg in num_segments[1:-1]:
+            seg_train = neo.SpikeTrain(time_stamps[(time_stamps > seg * inter) & (time_stamps < ((seg + 1) * inter))],
+                                       units='sec', t_start=(seg * inter), t_stop=((seg + 1) * inter))
+            new_block.segments[seg].spiketrains.append(seg_train)
 
-        last_seg = neo.SpikeTrain(st[st > (num_segments[-1] * inter)], units='sec',
+        last_seg = neo.SpikeTrain(time_stamps[time_stamps > (num_segments[-1] * inter)], units='sec',
                                   t_start=(num_segments[-1]) * inter, t_stop=((num_segments[-1] + 1) * inter))
         new_block.segments[num_segments[-1]].spiketrains.append(last_seg)
+        print(name)
+        print(new_block.size)
     return new_block
 
 
+a = (1, 2)
+str(a[0])
+cc = neo.ChannelIndex(name='1', index=)
+data_file = '/Users/felipeantoniomendezsalcido/Desktop/MEAs/data_files/CA3_WT_01117_merged_sp.txt'
+raw_data = pd.read_csv(data_file, sep=',', header=0, usecols=[0, 1, 2])
+ord_times = raw_data.groupby(['Channel', 'Unit'])['Timestamp']
+kiis = ord_times.groups.keys()
+for ii in kiis:
+    print(ii)
 n_block = build_block(data_file)
-
+n_block.segments
 # So we got the spike trains all tidy and nice in a block by segments
 # Now, characterize them
 # What are the most useful statistics to describe a spike train?
 # Firing rate, brust/regular, cv, network spikes, spikes in burst, burst rate
 
 # Build a DataFrame for that data
+
 
 stat_dic = {'F_Rate': (), 'CV': (), }
 for i in n_block.segments[0].spikestrains:
@@ -90,7 +92,7 @@ for i in range(lim):
         sim_mat[i, j] = sim
 
 plt.figure(figsize=(15, 10))
-sb.heatmap(sim_mat)
+sns.heatmap(sim_mat)
 plt.show()
 
 cc_mat = corrcoef(binned_trains)
@@ -98,14 +100,15 @@ np.fill_diagonal(cc_mat, 0)
 
 
 plt.figure(figsize=(10, 10))
-sb.heatmap(cc_mat, square=True, cmap='Spectral_r')
+sns.heatmap(cc_mat, square=True, cmap='Spectral_r')
 plt.ylabel('Cells')
 plt.xlabel('Cells')
 plt.show()
 
+len(n_block.segments[0].spiketrains)
 
 # Raster Plots
-plt.figure(figsize=(30, 30))
+plt.figure(figsize=(10, 5))
 plt.eventplot(n_block.segments[0].spiketrains, color='k', linelengths=0.5)
 #plt.eventplot(exp_block.segments[1].spiketrains, color='k')
 #plt.eventplot(exp_block.segments[2].spiketrains, color='k')
@@ -113,7 +116,7 @@ plt.eventplot(n_block.segments[0].spiketrains, color='k', linelengths=0.5)
 
 plt.xlabel('Seconds')
 plt.ylabel('Cell')
-plt.yticks(range(42))
+plt.yticks(range(len(n_block.segments[0].spiketrains)))
 plt.show()
 
 
