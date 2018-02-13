36cf2865239bc587a0e5f8abf45cde95726a7492
FMendezSlc <fmendezslc@gmail.com>
2018-02-12 19:11:40 -0600

    sttc function working
    Finished spike time tiling coefficient function working

--- MCS_spiketrains.py
+++ MCS_spiketrains.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from quantities import Hz, s, ms
 from elephant.conversion import BinnedSpikeTrain
+from elephant import spike_train_generation as sp_gen
 import seaborn as sns
 import elephant.statistics as est
 import elephant.spike_train_correlation as sp_cor
@@ -51,6 +52,74 @@ def build_block(data_file):
 
 ex_exp = build_block(
     '/Users/felipeantoniomendezsalcido/Desktop/MEAs/data_files/CA3_WT_fem_091117_merged_sp.txt')
+ex_exp.segments[0].spiketrains[0][-1]
+# Working function to calculate the Spike Time Tiling Coefficient
+# According to Cutts and Eglen (2014)
+# Original function written in C (github/CCutts)
+
+
+def sttc(spiketrain_1, spiketrain_2, dt=0.005 * s):
+    ''' Calculates the Spike Time Tiling Coefficient as described in (Cutss & Eglen, 2014) following Cutts' implementation in C (https://github.com/CCutts/Detecting_pairwise_correlations_in_spike_trains/blob/master/spike_time_tiling_coefficient.c)'''
+
+    def run_P(spiketrain_1, spiketrain_2, N1, N2, dt):
+        '''Check every spike in train 1 to see if there's a spike in train 2 within dt.'''
+        Nab = 0
+        j = 0
+        for i in range(N1):
+            while (j < N2):  # don't need to search all j each iteration
+                if np.abs(spiketrain_1[i] - spiketrain_2[j]) <= dt:
+                    Nab = Nab + 1
+                    break
+                elif spiketrain_2[j] > spiketrain_1[i]:
+                    break
+                else:
+                    j = j + 1
+        return Nab
+
+    def run_T(spiketrain, N, dt):
+        ''' Calculate the proportion of the total recording time 'tiled' by sipkes.'''
+        time_A = 2 * N * dt  # maxium possible time
+
+        if N == 1:  # for just one spike in train
+            if spiketrain[0] - spiketrain.t_start < dt:
+                time_A = time_A - dt + sipketrain[0] - spiketrain.t_start
+            elif spiketrain[0] + dt > spiketrain.t_stop:
+                time_A = time_A - dt - spiketrain[0] + spiketrain.t_stop
+
+        else:  # if more than one spike in train
+            i = 0
+            while (i < (N - 1)):
+                diff = spiketrain[i + 1] - spiketrain[i]
+
+                if diff < (2 * dt):  # subtract overlap
+                    time_A = time_A - 2 * dt + diff
+                i += 1
+                # check if spikes are within dt of the start and/or end
+                # if so just need to subract overlap of first and/or last spike
+            if (spiketrain[0] - spiketrain.t_start) < dt:
+                time_A = time_A + spiketrain[0] - dt - spiketrain.t_start
+
+            if (spiketrain.t_stop - spiketrain[N - 1]) < dt:
+                time_A = time_A - spiketrain[-1] - dt + spiketrain.t_stop
+
+            T = (time_A / spiketrain.t_stop).item()
+            return T
+
+    N1 = len(spiketrain_1)
+    N2 = len(spiketrain_2)
+
+    if N1 == 0 or N2 == 0:
+        index = np.nan
+    else:
+        TA = run_T(spiketrain_1, N1, dt)
+        TB = run_T(spiketrain_2, N2, dt)
+        PA = run_P(spiketrain_1, spiketrain_2, N1, N2, dt)
+        PA = PA / N1
+        PB = run_P(spiketrain_2, spiketrain_1, N2, N1, dt)
+        PB = PB / N2
+        index = 0.5 * (PA - TB) / (1 - TB * PA) + 0.5 * (PB - TA) / (1 - TA * PB)
+    return index
+
 
 os.chdir('/Users/felipeantoniomendezsalcido/Desktop/MEAs/data_files')
 # Exploratory dataframe for an experiment
@@ -112,7 +181,6 @@ for ii in range(egg_mat_size):
         cch_try = sp_cor.cch(few_trains[ii], few_trains[jj], cross_corr_coef=True)
         egg_mat[ii, jj] = max(cch_try)
 
-2 + 2
 egg_mat
 bin_mat = binned_trains.to_array()
 bin_mat.shape
@@ -144,8 +212,8 @@ plt.show()
 # Raster Plots
 plt.figure(figsize=(30, 30))
 plt.eventplot(n_block.segments[0].spiketrains, color='k', linelengths=0.5)
-#plt.eventplot(exp_block.segments[1].spiketrains, color='k')
-#plt.eventplot(exp_block.segments[2].spiketrains, color='k')
+# plt.eventplot(exp_block.segments[1].spiketrains, color='k')
+# plt.eventplot(exp_block.segments[2].spiketrains, color='k')
 plt.xlabel('Seconds')
 plt.ylabel('Cell')
 plt.yticks(range(len(n_block.segments[0].spiketrains)))
